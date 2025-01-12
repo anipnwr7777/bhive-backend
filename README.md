56ba3aca1eb4895137544b75b100f9ae60d54122
ANIRUDH PANWAR <anirudhpanwar7777@gmail.com>
2025-01-12 09:57:58 +0530

    Add migrations

---
+++ README.md
@@ -0,0 +1,23 @@
+# Mutual Fund Broker Web Application with RapidAPI Integration
+
+## Sample data from RapidAPI:
+### Latest data : 
+```
+    [
+        {
+            "Scheme_Code": 119551,
+            "ISIN_Div_Payout_ISIN_Growth": "INF209KA12Z1",
+            "ISIN_Div_Reinvestment": "INF209KA13Z9",
+            "Scheme_Name": "Aditya Birla Sun Life Banking & PSU Debt Fund  - DIRECT - IDCW",
+            "Net_Asset_Value": 102.5788,
+            "Date": "10-Jan-2025",
+            "Scheme_Type": "Open Ended Schemes",
+            "Scheme_Category": "Debt Scheme - Banking and PSU Fund",
+            "Mutual_Fund_Family": "Aditya Birla Sun Life Mutual Fund"
+        },
+        ...
+    ]
+```
+
+
+
---
+++ migrations/20250112042127_create_investments_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema.createTable('investments', (table) => {
+        table.increments('id').primary();
+        table.integer('user_id').unsigned().notNullable().references('id').inTable('users').onDelete('CASCADE');
+        table.string('scheme_code').notNullable(); 
+        table.float('purchase_nav').notNullable(); 
+        table.float('investment_amount').notNullable(); 
+        table.timestamp('purchase_date').defaultTo(knex.fn.now()); 
+        table.timestamps(true, true); 
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema.dropTable('investments');
+};
